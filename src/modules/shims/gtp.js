6c568f4bfd21a7d5a61110ceec6c43ea1535066d
Terkwood <metaterkhorn@gmail.com>
2019-07-07 15:34:06 -0400

    stub out controller.start

--- src/modules/shims/gtp.js
+++ src/modules/shims/gtp.js
@@ -1,5 +1,27 @@
 const EventEmitter = require('events')
 
-class Controller extends EventEmitter {}
+class Controller extends EventEmitter {
+    constructor(path, args = [], spawnOptions = {}) {
+        super()
+
+        this.path = path
+        this.args = args
+        this.spawnOptions = spawnOptions
+
+        this._streamController = null
+        this.process = null
+        this.commands = []
+    }
+
+    get busy() {
+        return this._streamController != null && this._streamController.busy
+    }
+
+    start() {
+        if (this.process != null) return
+
+        console.log("BEEP")  // TODO BUGOUT
+    }
+}
 
 exports.Controller = Controller
