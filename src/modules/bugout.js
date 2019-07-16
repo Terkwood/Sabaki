c01a540011e1dadb51a0925f30f361c8d4fa738d
Terkwood <metaterkhorn@gmail.com>
2019-07-16 17:37:48 -0400

    remove dead attempt

--- src/modules/bugout.js
+++
@@ -1,8 +0,0 @@
-exports.safeMoveNumber = 
-    (playerLetter, moveNumber) => {
-        if ((playerLetter === "B" && moveNumber % 2) 
-            || (playerLetter === "W" && !(moveNumber % 2)))
-            return moveNumber
-            
-        return moveNumber - 1
-    }
