acd47f06b870af95d5bced701e1d1923f2b8741e
Terkwood <metaterkhorn@gmail.com>
2019-08-16 14:27:47 -0400

    validate base62

--- src/modules/bugout.js
+++ src/modules/bugout.js
@@ -1,6 +1,10 @@
+const re = new RegExp(/^[a-zA-Z0-9]+$/, 'm')
+
+const isValidBase62 = p => re.test(p)
+
 const joinParam = () => {
     let urlParams = new URLSearchParams(window.location.search)
-    if (urlParams.has("join")) {
+    if (urlParams.has("join") && isValidBase62(urlParams.get("join"))) {
         return { join: true, gameId: urlParams.get("join") }
     } else {
         return { join: false }
