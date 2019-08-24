cc5b1911d4cf87757b53f5dd8be2e9f57fbfb942
Terkwood <metaterkhorn@gmail.com>
2019-08-24 11:05:35 -0400

    Add preact-material-components

--- webpack.config.js
+++ webpack.config.js
@@ -23,10 +23,10 @@ module.exports = (env, argv) => ({
 
     resolve: {
         alias: {
+            'react': path.join(__dirname, 'node_modules/preact/dist/preact.min'),
             // TODO bugout V
-            //'react': path.join(__dirname, 'node_modules/preact/dist/preact.min'),
-            'react': 'preact-compat',
-            'react-dom': 'preact-compat',
+            //TODO FAILED 'react': 'preact-compat',       // TODO direct load from node_modules fails - check publish strat
+            //TODO FAILED 'react-dom': 'preact-compat',   // TODO direct load from node_modules fails - check publish strat
             // TODO bugout ^
             'preact': path.join(__dirname, 'node_modules/preact/dist/preact.min'),
             'prop-types': path.join(__dirname, 'src/modules/shims/prop-types'),
