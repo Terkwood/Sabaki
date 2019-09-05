a460ec2be066b588f1ccbf902404a5c7eeaf4f13
Terkwood <metaterkhorn@gmail.com>
2019-09-05 17:20:15 -0400

    exclude most of preact-material-components

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,55 @@ module.exports = (env, argv) => ({
 
     devtool: argv.mode === 'production' ? false : 'cheap-module-eval-source-map',
 
+    module: {
+        rules: [
+          {
+            exclude: [
+                './node_modules/preact-material-components/Base',
+                './node_modules/preact-material-components/Button',
+                './node_modules/preact-material-components/Card',
+                './node_modules/preact-material-components/Checkbox',
+                './node_modules/preact-material-components/Chips',
+                './node_modules/preact-material-components/Drawer',
+                './node_modules/preact-material-components/Elevation',
+                './node_modules/preact-material-components/Fab',
+                './node_modules/preact-material-components/FormField',
+                './node_modules/preact-material-components/GridList',
+                './node_modules/preact-material-components/Icon',
+                './node_modules/preact-material-components/IconButton',
+                './node_modules/preact-material-components/IconToggle',
+                './node_modules/preact-material-components/ImageList',
+                './node_modules/preact-material-components/LayoutGrid',
+                './node_modules/preact-material-components/LineRipple',
+                './node_modules/preact-material-components/LinearProgress',
+                './node_modules/preact-material-components/List',
+                './node_modules/preact-material-components/Menu',
+                './node_modules/preact-material-components/MenuSurface',
+                './node_modules/preact-material-components/Radio',
+                './node_modules/preact-material-components/Select',
+                './node_modules/preact-material-components/Slider',
+                './node_modules/preact-material-components/Snackbar',
+                './node_modules/preact-material-components/Switch',
+                './node_modules/preact-material-components/Tab',
+                './node_modules/preact-material-components/TabBar',
+                './node_modules/preact-material-components/TabIndicator',
+                './node_modules/preact-material-components/TabScroller',
+                './node_modules/preact-material-components/Tabs',
+                './node_modules/preact-material-components/TextField',
+                './node_modules/preact-material-components/Theme',
+                './node_modules/preact-material-components/Toolbar',
+                './node_modules/preact-material-components/TopAppBar',
+                './node_modules/preact-material-components/Typography',
+                './node_modules/preact-material-components/dist',
+                './node_modules/preact-material-components/esm',
+                './node_modules/preact-material-components/scripts',
+                './node_modules/preact-material-components/themeUtils',
+                './node_modules/preact-material-components/ts'
+            ]
+          }
+        ]
+    },
+
     node: {
         Buffer: false
     },
