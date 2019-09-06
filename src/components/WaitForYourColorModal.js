861a0111f365fc471f49bf97f6ba814b51dda606
Terkwood <metaterkhorn@gmail.com>
2019-09-06 14:04:42 -0400

    Create WaitForYourColorModal.js

---
+++ src/components/WaitForYourColorModal.js
@@ -0,0 +1,29 @@
+const { h, Component } = require('preact')
+
+// 🦹🏻‍ Bundle Bloat Protector
+import Dialog from 'preact-material-components/Dialog'
+
+class WaitForYourColorModal extends Component {
+    constructor() {
+        super()
+    }
+
+    render({ 
+        id = 'wait-for-opponent-modal', 
+        data
+    }) {
+
+        return undefined != data && !data.wait ?
+           h(Dialog,
+                {
+                    id,
+                    isOpen: true,
+                },
+                h(Dialog.Header, null, 'Please Wait' ),
+                h(Dialog.Body, null, 'WAITING FOR YOUR COLOR')
+            )
+        : h('div', { id })
+    }
+}
+
+export default WaitForYourColorModal
