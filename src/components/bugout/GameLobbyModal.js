4f8bc12a1f4186a6d1b6de143dd7e8e1d85006d0
Terkwood <metaterkhorn@gmail.com>
2019-12-30 12:49:19 -0500

    Update GameLobbyModal.js

--- src/components/bugout/GameLobbyModal.js
+++ src/components/bugout/GameLobbyModal.js
@@ -25,7 +25,7 @@ class GameLobbyModal extends Component {
                     isOpen: true,
                 },
                 h(Dialog.Header, null, "Join Private Game"),
-                h(Dialog.Body, null, "🐛 Welcome to BUGOUT! You're joining a private game created by your friend."),
+                h(Dialog.Body, null, "🐛 Welcome to BUGOUT! You're joining a  game created by your friend."),
                 h(Dialog.Footer, null, 
                     h(Dialog.FooterButton, 
                         { 
@@ -46,7 +46,7 @@ class GameLobbyModal extends Component {
                 isOpen: true,
             },
             h(Dialog.Header, null, "Choose Venue"),
-            h(Dialog.Body, null, "🐛 Welcome to BUGOUT! You may find a public game with the next available player, or create a private game and share its link with your friend."),
+            h(Dialog.Body, null, "QUICK GAME: join the next available player, 19x19 board only. CREATE GAME: invite via URL, choose board size."),
             h(Dialog.Footer, null, 
                 h(Dialog.FooterButton, 
                     { 
@@ -56,7 +56,7 @@ class GameLobbyModal extends Component {
                             update(EntryMethod.FIND_PUBLIC)
                         }
                     }, 
-                    "Public")
+                    "Quick Game")
                 ),
             h(Dialog.Footer, null, 
                 h(Dialog.FooterButton, 
@@ -66,7 +66,7 @@ class GameLobbyModal extends Component {
                             this.setState({showDialog: false})
                             update(EntryMethod.CREATE_PRIVATE)
                         }
-                    }, "Private"))
+                    }, "Create Game"))
         ) : empty
     }
 }
