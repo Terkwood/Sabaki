e7ec5ce6ebb94ed41542505e895219f0576704f6
Terkwood <metaterkhorn@gmail.com>
2020-01-04 08:42:40 -0500

    Default to no move colorization

--- src/setting.js
+++ src/setting.js
@@ -104,7 +104,7 @@ let defaults = {
     'view.show_comments': true,
     'view.show_coordinates': false,
     'view.show_graph': true,
-    'view.show_move_colorization': true,
+    'view.show_move_colorization': false,
     'view.show_move_numbers': false,
     'view.show_next_moves': true,
     'view.show_siblings': true,
